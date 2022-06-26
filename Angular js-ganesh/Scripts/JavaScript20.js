44baaca7ecf53161d2347eb6e800b55c8d9f06a6
Ganesan N <ganesan.n@ivygdcad.ivy-support.com>
2022-06-26 19:56:44 +0530

    project

---
+++ Angular js-ganesh/Scripts/JavaScript20.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="angular.min.js" />
+
+
+
+var app = angular
+    .module("myModule", [])
+    .controller("myController", function ($scope, stringService) {
+        $scope.transformString = function (input) {
+            $scope.output = stringService.processString(input);
+        };
+    });;
---
+++ Angular js-ganesh/Scripts/JavaScript22.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="angular.min.js" />
+
+
+var demoApp = angular.module("demoApp", [])
+    .controller("countryController",
+        function ($scope, $location, $anchorScroll, $http) {
+            $http.get("CountryService.asmx/GetData")
+                .then(function (response) {
+                    $scope.countries = response.data;
+                });
+
+            $scope.scrollTo = function (countryName) {
+                $location.hash(countryName);
+                $anchorScroll();
+            }
+
+        });
